3d0da46cb13d242c9e42501126d1aa28bd1c3297
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-07-01 00:30:51 -0400

    Update HexGameBoard.java

--- src/main/java/com/atraparalagato/impl/model/HexGameBoard.java
+++ src/main/java/com/atraparalagato/impl/model/HexGameBoard.java
@@ -1,4 +1,4 @@
-// HexGameBoard.java
+
 package com.atraparalagato.impl.model;
 
 import com.atraparalagato.base.model.GameBoard;
@@ -8,45 +8,52 @@ import java.util.function.Predicate;
 
 public class HexGameBoard extends GameBoard<HexPosition> {
 
-    private final int size = 11;
-    private final Set<HexPosition> blockedPositions = new HashSet<>();
+    private int boardSize;
+    private Set<HexPosition> blocked = new HashSet<>();
+
+    public HexGameBoard(int boardSize) {
+        this.boardSize = boardSize;
+        this.blocked = initializeBlockedPositions();
+    }
 
     @Override
     protected Set<HexPosition> initializeBlockedPositions() {
-        return blockedPositions;
+        return new HashSet<>();
+    }
+
+    public void setBlockedPositions(Set<HexPosition> positions) {
+        this.blocked = positions;
     }
 
     @Override
     public boolean isPositionInBounds(HexPosition position) {
         int q = position.getQ();
         int r = position.getR();
-        return q >= 0 && q < size && r >= 0 && r < size;
+        return q >= 0 && q < boardSize && r >= 0 && r < boardSize;
     }
 
     @Override
     public boolean isValidMove(HexPosition position) {
-        return isPositionInBounds(position) && !blockedPositions.contains(position);
+        return isPositionInBounds(position) && !blocked.contains(position);
     }
 
     @Override
     public void executeMove(HexPosition position) {
-        if (isValidMove(position)) {
-            blockedPositions.add(position);
-        }
+        blocked.add(position);
     }
 
     @Override
     public List<HexPosition> getPositionsWhere(Predicate<HexPosition> condition) {
-        List<HexPosition> positions = new ArrayList<>();
-        for (int q = 0; q < size; q++) {
-            for (int r = 0; r < size; r++) {
+        List<HexPosition> result = new ArrayList<>();
+        for (int q = 0; q < boardSize; q++) {
+            for (int r = 0; r < boardSize; r++) {
                 HexPosition pos = new HexPosition(q, r);
                 if (condition.test(pos)) {
-                    positions.add(pos);
+                    result.add(pos);
                 }
             }
         }
-        return positions;
+        return result;
     }
 
     @Override
@@ -55,19 +62,23 @@ public class HexGameBoard extends GameBoard<HexPosition> {
             {+1,  0}, {+1, -1}, { 0, -1},
             {-1,  0}, {-1, +1}, { 0, +1}
         };
-
-        List<HexPosition> adjacents = new ArrayList<>();
+        List<HexPosition> adj = new ArrayList<>();
         for (int[] dir : directions) {
             HexPosition neighbor = new HexPosition(position.getQ() + dir[0], position.getR() + dir[1]);
-            if (isPositionInBounds(neighbor) && !blockedPositions.contains(neighbor)) {
-                adjacents.add(neighbor);
+            if (isValidMove(neighbor)) {
+                adj.add(neighbor);
             }
         }
-        return adjacents;
+        return adj;
     }
 
     @Override
     public boolean isBlocked(HexPosition position) {
         return getAdjacentPositions(position).isEmpty();
+    }
+
+    @Override
+    public int getBoardSize() {
+        return boardSize;
     }
 }
