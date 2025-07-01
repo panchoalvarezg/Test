d295849bb40811ec20b70d0f4fd49973fdc4af3d
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-07-01 01:28:19 -0400

    Update HexGameService.java

--- src/main/java/com/atraparalagato/impl/service/HexGameService.java
+++ src/main/java/com/atraparalagato/impl/service/HexGameService.java
@@ -1,25 +1,50 @@
-package com.atraparalagato.impl.strategy;
+package com.atraparalagato.impl.service;
 
-import com.atraparalagato.base.strategy.CatMovementStrategy;
 import com.atraparalagato.impl.model.HexPosition;
 import com.atraparalagato.impl.model.HexGameBoard;
+import com.atraparalagato.impl.model.HexGameState;
+import com.atraparalagato.base.repository.DataRepository;
+import com.atraparalagato.base.strategy.CatMovementStrategy;
 import java.util.*;
 
-public class BFSCatMovement implements CatMovementStrategy<HexPosition> {
-    @Override
-    public List<HexPosition> getPossibleMoves(HexPosition from, Object board) {
-        HexGameBoard hexBoard = (HexGameBoard) board;
-        List<HexPosition> moves = new ArrayList<>();
-        for (HexPosition neighbor : hexBoard.getAdjacentPositions(from)) {
-            if (!hexBoard.isBlocked(neighbor)) {
-                moves.add(neighbor);
-            }
-        }
-        return moves;
+public class HexGameService {
+
+    private final DataRepository<HexGameState, String> repository;
+    private final CatMovementStrategy<HexPosition> catStrategy;
+    private HexGameState currentState;
+
+    public HexGameService(DataRepository<HexGameState, String> repository,
+                          CatMovementStrategy<HexPosition> catStrategy,
+                          int boardSize,
+                          HexPosition catStart,
+                          String gameId) {
+        this.repository = repository;
+        this.catStrategy = catStrategy;
+        HexGameBoard board = new HexGameBoard(boardSize);
+        this.currentState = new HexGameState(gameId, board, catStart);
     }
 
-    @Override
-    public HexPosition selectBestMove(HexPosition from, Object board, List<HexPosition> possibleMoves) {
-        return possibleMoves.isEmpty() ? null : possibleMoves.get(0);
+    public boolean executeMove(HexPosition pos) {
+        boolean result = currentState.executeMove(pos);
+        if (result) catMove();
+        return result;
+    }
+
+    public void catMove() {
+        List<HexPosition> moves = catStrategy.getPossibleMoves(currentState.getCatPosition(), currentState.getBoard());
+        HexPosition best = catStrategy.selectBestMove(currentState.getCatPosition(), currentState.getBoard(), moves);
+        if (best != null && !currentState.getBoard().isBlocked(best)) {
+            currentState.setCatPosition(best);
+            currentState.updateGameStatus();
+        }
+    }
+
+    public HexPosition getSuggestedMove() {
+        List<HexPosition> moves = catStrategy.getPossibleMoves(currentState.getCatPosition(), currentState.getBoard());
+        return catStrategy.selectBestMove(currentState.getCatPosition(), currentState.getBoard(), moves);
+    }
+
+    public HexPosition getTargetPosition() {
+        return currentState.getCatPosition();
     }
 }
